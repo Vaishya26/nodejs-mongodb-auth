882df4482c6704fc9248cc201bd1bd08c250e22d
Vaishya26 <vaishya.arpit9@gmail.com>
2020-07-22 22:52:06 +0530

    updated features

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var http = require('http');
 var os = require('os');
-var socketIO = require('socket.io');
 const express = require("express");
 const { check, validationResult} = require("express-validator");
 const bodyParser = require("body-parser");
@@ -8,14 +7,14 @@ const cookieParser = require('cookie-parser');
 const bcrypt = require("bcryptjs");
 const session = require('express-session');
 const MongoClient = require('mongodb').MongoClient;
-const url = "mongodb+srv://ajnabuild:ajnabuildauth@ajnabuild.ypts7.mongodb.net/";
+const url = "mongodb+srv://<username>:<password>@<cluster>.ypts7.mongodb.net/";
 const app = express();
 
 
 //------Initializing Server------------------------    
 var server = http.createServer(app);
 server.listen(process.env.PORT || 8000);
-var io = socketIO(server);
+
 
 var myAwesomeDB;
 
@@ -161,7 +160,7 @@ app.route('/login')
 app.get('/dashboard', (req, res) => {
     if (req.session.loggedinUser && req.cookies.user_sid) {
         // res.sendFile(__dirname + '/public/dashboard.html');
-        res.render("webrtc.ejs");
+        res.render("dashboard.ejs");
     } else {
         res.redirect('/login');
     }
@@ -183,59 +182,3 @@ app.get('/logout', (req, res) => {
 
 
 
-//------Socket.IO connections------------------
-io.sockets.on('connection', function(socket) {
-    // convenience function to log server messages on the client
-    // arguments is an array like object which contains all the arguments of log(). 
-    // to push all the arguments of log() in array, we have to use apply().
-    function log() {
-      var array = ['Message from server:'];
-      array.push.apply(array, arguments);
-      socket.emit('log', array);
-    }
-  
-    socket.on('message', function(message, room) {
-      log('Client said: ', message);
-      // for a real app, would be room-only (not broadcast)
-      socket.in(room).emit('message', message, room);
-    });
-  
-    socket.on('create or join', function(room) {
-      log('Received request to create or join room ' + room);
-  
-      var clientsInRoom = io.sockets.adapter.rooms[room];
-      var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
-      log('Room ' + room + ' now has ' + numClients + ' client(s)');
-  
-      if (numClients === 0) {
-        socket.join(room);
-        log('Client ID ' + socket.id + ' created room ' + room);
-        socket.emit('created', room, socket.id);
-  
-      } else if (numClients === 1) {
-        log('Client ID ' + socket.id + ' joined room ' + room);
-        io.sockets.in(room).emit('join', room);
-        socket.join(room);
-        socket.emit('joined', room, socket.id);
-        io.sockets.in(room).emit('ready');
-      } else { // max two clients
-        socket.emit('full', room);
-      }
-    });
-  
-    socket.on('ipaddr', function() {
-      var ifaces = os.networkInterfaces();
-      for (var dev in ifaces) {
-        ifaces[dev].forEach(function(details) {
-          if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
-            socket.emit('ipaddr', details.address);
-          }
-        });
-      }
-    });
-  
-    socket.on('bye', function(){
-      console.log('received bye');
-    });
-  
-  });
